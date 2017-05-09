12e0e85ce341c299dee13bffe231515746effb5d
Dave Strus <dave@getfretless.com>
2017-05-09 13:59:03 -0400

    Add values to definition list

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const personForm = document.querySelector('form')
-const details = document.querySelector('.details')
 
 const handleSubmit = (ev) => {
   ev.preventDefault()
-  const firstName = ev.target.firstName.value
-  console.log(firstName)
-  // details.textContent = firstName
-  // details.innerHTML = `<strong>${firstName}</strong>`
+  const form = ev.target
+  const details = document.querySelector('.details')
+  const firstName = form.firstName.value
+  const hairColor = form.hairColor.value
+  const age = form.age.value
+  const birthplace = form.birthplace.value
 
-  const em = document.createElement('em')
-  em.textContent = firstName
-  details.appendChild(em)
+  details.innerHTML = `
+    <dl>
+      <li>
+        <dt>First Name</dt>
+        <dd>${firstName}</dd>
+      </li>
+      <li>
+        <dt>Hair Color</dt>
+        <dd>${hairColor}</dd>
+      </li>
+      <li>
+        <dt>Age</dt>
+        <dd>${age}</dd>
+      </li>
+      <li>
+        <dt>Birthplace</dt>
+        <dd>${birthplace}</dd>
+      </li>
+    </dl>
+  `
 }
 
 personForm.addEventListener('submit', handleSubmit)
