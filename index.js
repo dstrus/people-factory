a170f0b708a077cf32481a64236ef11d31931dc0
Dave Strus <dave@getfretless.com>
2017-05-09 13:44:43 -0400

    Create and append DOM elements

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const personForm = document.querySelector('form')
+const details = document.querySelector('.details')
 function handleSubmit(ev) {
   ev.preventDefault()
-  console.log({ev})
-  console.log(ev.target)
-  console.log({ this: this })
+  console.log(this.firstName.value)
+  // details.textContent = this.firstName.value
+  // details.innerHTML = `<strong>${this.firstName.value}</strong>`
+  
+  const em = document.createElement('em')
+  em.textContent = this.firstName.value
+  details.appendChild(em)
 }
 
 personForm.onsubmit = handleSubmit
