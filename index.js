72ef2efab7d805cfb19fbb3e1dafaddae8875eec
Dave Strus <dave@getfretless.com>
2017-05-09 14:31:07 -0400

    Wrap it in an IIFE

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-const personForm = document.querySelector('form')
+(function() {
+  const personForm = document.querySelector('form')
 
-const renderColor = (color) => {
-  const colorDiv = document.createElement('div')
-  colorDiv.style.height = '100px'
-  colorDiv.style.width = '100px'
-  colorDiv.style.backgroundColor = color
-  return colorDiv.outerHTML
-}
-
-const buildListItem = (term, definition) => {
-  const li = document.createElement('li')
-  const dt = document.createElement('dt')
-  const dd = document.createElement('dd')
-  dt.textContent = term
-  dd.innerHTML = definition
-  li.appendChild(dt)
-  li.appendChild(dd)
-  return li
-}
-
-const buildList = (listValues) => {
-  const dl = document.createElement('dl')
-  dl.style.border = '1px solid #1779ba'
-  dl.appendChild(buildListItem('Name', listValues.firstName))
-  dl.appendChild(buildListItem('Hair Color', listValues.hairColor))
-  dl.appendChild(buildListItem('Age', listValues.age))
-  dl.appendChild(buildListItem('Birthplace', listValues.birthplace))
-
-  return dl
-}
-
-const handleSubmit = (ev) => {
-  ev.preventDefault()
-  const form = ev.target
-  const details = document.querySelector('.details')
-
-  const formValues = {
-    firstName: form.firstName.value,
-    hairColor: renderColor(form.hairColor.value),
-    age: form.age.value,
-    birthplace: form.birthplace.value,
+  const renderColor = (color) => {
+    const colorDiv = document.createElement('div')
+    colorDiv.style.height = '100px'
+    colorDiv.style.width = '100px'
+    colorDiv.style.backgroundColor = color
+    return colorDiv.outerHTML
   }
 
-  details.appendChild(buildList(formValues))
-}
+  const buildListItem = (term, definition) => {
+    const li = document.createElement('li')
+    const dt = document.createElement('dt')
+    const dd = document.createElement('dd')
+    dt.textContent = term
+    dd.innerHTML = definition
+    li.appendChild(dt)
+    li.appendChild(dd)
+    return li
+  }
 
-personForm.addEventListener('submit', handleSubmit)
+  const buildList = (listValues) => {
+    const dl = document.createElement('dl')
+    dl.style.border = '1px solid #1779ba'
+    dl.appendChild(buildListItem('Name', listValues.firstName))
+    dl.appendChild(buildListItem('Hair Color', listValues.hairColor))
+    dl.appendChild(buildListItem('Age', listValues.age))
+    dl.appendChild(buildListItem('Birthplace', listValues.birthplace))
+
+    return dl
+  }
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    const form = ev.target
+    const details = document.querySelector('.details')
+
+    const formValues = {
+      firstName: form.firstName.value,
+      hairColor: renderColor(form.hairColor.value),
+      age: form.age.value,
+      birthplace: form.birthplace.value,
+    }
+
+    details.appendChild(buildList(formValues))
+  }
+
+  personForm.addEventListener('submit', handleSubmit)
+})()
